55c3864135c0156556457bc82284ecd4209ffa1f
headbwoi <tochukwujohn24@gmail.com>
2022-07-25 07:59:49 +0100

    finished 8/9 of features in readme

--- script.js
+++ script.js
@@ -39,30 +39,50 @@ const changeTheme = (svg) => {
     localStorage.setItem("theme", JSON.stringify(null))
   }
 }
-
 // parent todo list items
 const ul = document.querySelector("ul")
 
+let todos = JSON.parse(localStorage.getItem("todo-list")) //fetches the todos onload
 //input
 const userInput = document.querySelector("#textinput")
+const itemsLeft = document.querySelector("[data-itemleft]")
+const clearCompleted = document.querySelector("[data-clearCompeted]")
 
-let todos = JSON.parse(localStorage.getItem("todo-list")) //fetches the todos onload
+let statusItems
+if (window.screen.width < 768) {
+  statusItems = document.querySelectorAll(".status-mobile p")
+} else if (window.screen.width > 768) {
+  statusItems = document.querySelectorAll(".status p")
+}
+
+const removeActiveClass = () => {
+  statusItems.forEach((item) => {
+    item.classList.remove("active")
+  })
+}
+statusItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    removeActiveClass()
+    item.classList.add("active")
+    showTodo(item.innerText.toLowerCase())
+  })
+})
 
 //shos the todo when called
-const showTodo = () => {
+const showTodo = (filter) => {
   let li = ""
 
   if (todos) {
     todos.forEach((todo, index) => {
       //if todos at the index of the clicked todo status is completed add checked to the input element else add ""
-      let isCompleted = todos[index].status == "completed" ? "checked" : ""
-
-      li += `
+      let isCompleted = todo.status == "completed" ? "checked" : ""
+      if (filter == todo.status || filter == "all") {
+        li += `
           <li
               class="flex items-center justify-start h-[3.25rem] md:h-[4.0625rem] pl-6 pr-5 border-b border-Light_Grayish_Blue_light dark:border-b dark:border-Very_Dark_Grayish_Blue_dark1 relative overflow-hidden group cursor-pointer"
             >
               <label for="${index}">
-                <input type="checkbox" id="${index}" class="todo hidden" ${isCompleted}/>
+                <input type="checkbox" id="${index}" class="todo hidden" ${isCompleted} onclick="updateStatus(this)"/>
                 <span
                   class="absolute top-1/2 -translate-y-1/2 left-4 md:left-6 mr-3 md:mr-6 w-5 h-5 md:h-6 md:w-6 rounded-full ring-1 ring-Light_Grayish_Blue_light dark:ring-Very_Dark_Grayish_Blue_dark2 cursor-pointer grid place-items-center group-hover:border-l group-hover:border-l-gradient_1 group-hover:border-r group-hover:border-r-gradient_2"
                 >
@@ -90,7 +110,7 @@ const showTodo = () => {
             <!-- check icon -->
               <div
                 class="ml-auto h-6 w-6 relative md:opacity-0 group-hover:opacity-100 check"
-                onclick="deleteTodo(${index})"
+                onclick="deleteTodo(${index}, ${filter})"
               >
                 <span
                   class="absolute top-1/2 -translate-y-1/2 w-full h-[1px] cursor-pointer bg-Light_Grayish_Blue_light dark:bg-Very_Dark_Grayish_Blue_dark1 block rotate-45"
@@ -101,35 +121,38 @@ const showTodo = () => {
               </div>
         </li>
    `
+      }
+
       ul.innerHTML = li
     })
   }
+  itemsLeft.innerText = todos.length
 }
-showTodo()
-// looping through todo items and adding active class and passing clicked to updateStatus function
-const todoItems = document.querySelectorAll(".todo")
-todoItems.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    item.classList.toggle("active")
-    updateStatus(e.target)
-  })
-})
-//updateStatus function
+showTodo("all")
+
 const updateStatus = (selectedTask) => {
   if (selectedTask.checked) {
+    selectedTask.classList.add("active")
     //updates selectedTask status to completed
     todos[selectedTask.id].status = "completed"
   } else {
-    //updates selectedTask status to completed
-    todos[selectedTask.id].status = "pending"
+    selectedTask.classList.remove("active")
+    //updates selectedTask status to active
+    todos[selectedTask.id].status = "active"
   } //updates the localstorage
   localStorage.setItem("todo-list", JSON.stringify(todos))
 }
 
-const deleteTodo = (deleteIndex) => {
-  todos.splice(deleteIndex, 1) //removes/deletes todo at the clicked index
+clearCompleted.addEventListener("click", () => {
+  todos.splice(0, todos.length) //removes/deletes todo at the clicked index
   localStorage.setItem("todo-list", JSON.stringify(todos)) //updates the todos in localstorage
   showTodo()
+})
+
+const deleteTodo = (deleteIndex, filter) => {
+  todos.splice(deleteIndex, 1) //removes/deletes todo at the clicked index
+  localStorage.setItem("todo-list", JSON.stringify(todos)) //updates the todos in localstorage
+  showTodo("all")
 }
 
 //receives input as you type
@@ -138,15 +161,21 @@ userInput.addEventListener("keyup", (e) => {
 
   if (e.key == "Enter" && userInputTask) {
     //makes sure the user enters something before submitting
-    // if (!todos) {
-    //   todos = []
-    // }
+
     !todos ? (todos = []) : todos //if there are no todos render empty array else render the todos
     userInput.value = "" //resets the user input
-    let userTask = { name: userInputTask, status: "pending" } //creates todo object
+    let userTask = { name: userInputTask, status: "active" } //creates todo object
     todos.push(userTask) //pushes todo object to array
 
     localStorage.setItem("todo-list", JSON.stringify(todos)) //sets the item in localstorage
   }
-  showTodo() //calls showTodo function
+  if (window.screen.width < 768) {
+    showTodo(
+      document.querySelector(".status-mobile p.active").innerText.toLowerCase()
+    )
+  } else if (window.screen.width > 768) {
+    showTodo(document.querySelector(".status p.active").innerText.toLowerCase())
+  }
+
+  //calls showTodo function
 })

0bcd6af1a72802b918ba242c0af12f28bb9bee3d
headbwoi <tochukwujohn24@gmail.com>
2022-07-25 06:46:35 +0100

    finished delete feature

--- script.js
+++ script.js
@@ -40,23 +40,113 @@ const changeTheme = (svg) => {
   }
 }
 
+// parent todo list items
+const ul = document.querySelector("ul")
+
 //input
 const userInput = document.querySelector("#textinput")
 
-let todos = JSON.parse(localStorage.getItem("todo-list"))
+let todos = JSON.parse(localStorage.getItem("todo-list")) //fetches the todos onload
 
+//shos the todo when called
+const showTodo = () => {
+  let li = ""
+
+  if (todos) {
+    todos.forEach((todo, index) => {
+      //if todos at the index of the clicked todo status is completed add checked to the input element else add ""
+      let isCompleted = todos[index].status == "completed" ? "checked" : ""
+
+      li += `
+          <li
+              class="flex items-center justify-start h-[3.25rem] md:h-[4.0625rem] pl-6 pr-5 border-b border-Light_Grayish_Blue_light dark:border-b dark:border-Very_Dark_Grayish_Blue_dark1 relative overflow-hidden group cursor-pointer"
+            >
+              <label for="${index}">
+                <input type="checkbox" id="${index}" class="todo hidden" ${isCompleted}/>
+                <span
+                  class="absolute top-1/2 -translate-y-1/2 left-4 md:left-6 mr-3 md:mr-6 w-5 h-5 md:h-6 md:w-6 rounded-full ring-1 ring-Light_Grayish_Blue_light dark:ring-Very_Dark_Grayish_Blue_dark2 cursor-pointer grid place-items-center group-hover:border-l group-hover:border-l-gradient_1 group-hover:border-r group-hover:border-r-gradient_2"
+                >
+                  <svg
+                    class="text-Very_Light_Gray dark:text-Very_Dark_Desaturated_Blue"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="11"
+                    height="9"
+                  >
+                    <path
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      d="M1 4.304L3.696 7l6-6"
+                    />
+                  </svg>
+                </span>
+
+                <p
+                  class="text-Very_Dark_Grayish_Blue_light dark:text-Light_Grayish_Blue_dark hover:dark:text-Light_Grayish_Blue_hover md:text-[1.09375rem] first-letter:uppercase relative pl-8 md:pl-12 font-normal cursor-pointer"
+                >
+                  ${todo.name}
+                </p>
+              </label>
+            <!-- check icon -->
+              <div
+                class="ml-auto h-6 w-6 relative md:opacity-0 group-hover:opacity-100 check"
+                onclick="deleteTodo(${index})"
+              >
+                <span
+                  class="absolute top-1/2 -translate-y-1/2 w-full h-[1px] cursor-pointer bg-Light_Grayish_Blue_light dark:bg-Very_Dark_Grayish_Blue_dark1 block rotate-45"
+                ></span>
+                <span
+                  class="absolute top-1/2 -translate-y-1/2 w-full h-[1px] cursor-pointer bg-Light_Grayish_Blue_light dark:bg-Very_Dark_Grayish_Blue_dark1 block -rotate-45"
+                ></span>
+              </div>
+        </li>
+   `
+      ul.innerHTML = li
+    })
+  }
+}
+showTodo()
+// looping through todo items and adding active class and passing clicked to updateStatus function
+const todoItems = document.querySelectorAll(".todo")
+todoItems.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    item.classList.toggle("active")
+    updateStatus(e.target)
+  })
+})
+//updateStatus function
+const updateStatus = (selectedTask) => {
+  if (selectedTask.checked) {
+    //updates selectedTask status to completed
+    todos[selectedTask.id].status = "completed"
+  } else {
+    //updates selectedTask status to completed
+    todos[selectedTask.id].status = "pending"
+  } //updates the localstorage
+  localStorage.setItem("todo-list", JSON.stringify(todos))
+}
+
+const deleteTodo = (deleteIndex) => {
+  todos.splice(deleteIndex, 1) //removes/deletes todo at the clicked index
+  localStorage.setItem("todo-list", JSON.stringify(todos)) //updates the todos in localstorage
+  showTodo()
+}
+
+//receives input as you type
 userInput.addEventListener("keyup", (e) => {
-  let userInputTask = userInput.value.trim()
+  let userInputTask = userInput.value.trim() //trims value
 
   if (e.key == "Enter" && userInputTask) {
-    if (!todos) {
-      todos = [];
-    }
-    userInput.value = "";
-    let userTask = { name: userInputTask, status: "pending" }
-    todos.push(userTask)
+    //makes sure the user enters something before submitting
+    // if (!todos) {
+    //   todos = []
+    // }
+    !todos ? (todos = []) : todos //if there are no todos render empty array else render the todos
+    userInput.value = "" //resets the user input
+    let userTask = { name: userInputTask, status: "pending" } //creates todo object
+    todos.push(userTask) //pushes todo object to array
 
-    localStorage.setItem("todo-list", JSON.stringify(todos))
+    localStorage.setItem("todo-list", JSON.stringify(todos)) //sets the item in localstorage
   }
-  showTodo()
+  showTodo() //calls showTodo function
 })
